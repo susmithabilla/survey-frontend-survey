e1aa448376007af77c67d7b4e6de8bee5b72ae6f
susmithabilla <susmitha.billa@eagles.oc.edu>
2022-07-25 21:10:42 -0500

    updated services

--- src/services/surveydata.js
+++ src/services/surveydata.js
@@ -1,22 +1,31 @@
-import http from "../http";
-class SurveyData {
-  getAll() {
-    return http.get("/surveys");
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080/api/surveys';
+
+
+class SurveyService {
+
+  getAll(){
+    return axios.get(API_URL);
   }
 
-  createSurvey(name,data) {
-    return http.post(`/surveys?title=${name}&UserId=1`, data);
+  create(title,data,uid,name) {
+    return axios.post(API_URL+`?title=${title}&userId=${uid}&username=${name}`, data);
   }
-  get(id) {
-    return http.get(`/surveys/${id}`);
+  get(sid) {
+    return axios.get(API_URL+`/${sid}`);
   }
 
-  update(id, name, data) {
-    return http.put(`/surveys/${id}?title=${name}`, data);
+  update(sid,title,data, uid, name) {
+    return axios.put(API_URL+`/${sid}?title=${title}&userId=${uid}&username=${name}`, data);
   }
   delete(id) {
-    return http.delete(`/surveys/${id}`);
+    return axios.delete(API_URL+`/${id}`);
+  }
+
+  findSurvey(title){
+    return axios.get(API_URL + `?title=${title}`);
   }
 }
 
-export default new SurveyData();
+export default new SurveyService();
